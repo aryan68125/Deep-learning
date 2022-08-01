702c1a3f5890215bfffeccb040a272a915573376
Aditya Kumar <aryan68125@gmail.com>
2022-08-01 23:25:46 +0530

    added a forward function

---
+++ deep_learning_prediction/feed_forward_calculation.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#let's create 500 samples per class
+#So what we are gonna do is generate some gaussian clouds
+Nclass = 500
+#So we are gonna have 3 gaussian clouds
+#np.random.randn(Nclass, 2) + np.array([0,-2]) So the 1st gaussian cloud is gonna centered at 0,-2
+X1 = np.random.randn(Nclass, 2) + np.array([0,-2])
+#np.random.randn(Nclass, 2) + np.array([2,2]) So the 2nd gaussian cloud is gonna centered at 2,2
+X2 = np.random.randn(Nclass, 2) + np.array([2,2])
+#np.random.randn(Nclass, 2) + np.array([-2,2]) So the 3rd gaussian cloud is gonna centered at -2,2
+X3 = np.random.randn(Nclass, 2) + np.array([-2,2])
+X = np.vstack([X1,X2,X3])
+
+#create our labels
+Y = np.array([0]*Nclass + [1]*Nclass + [2]*Nclass)
+
+#visualizing the created data points on matplotlib
+plt.scatter(X[:,0], X[:,1], c=Y, s=100, alpha=0.5)
+plt.show()
+
+#Number of inputs
+D=2
+#Number of hidden layers
+M=3
+#number of classes
+K=3
+
+#initialize the weights
+#On the input side
+#W1 weight matrix has to be D x M matrix
+W1 = np.random.randn(D,M)
+#b1 bias term
+b1 = np.random.randn(M)
+#On the output side
+#M2 weight matrix has to be M X K matrix
+W2 = np.random.randn(M,K)
+#b2 bias term
+b2 = np.random.randn(K)
+
+#let's define the forward action of the neural network
+#It's gonna take in x matrix , W1, b1, W2, b2
+def forward(X,W1,b1,W2,b2):
+    # we are gonna use sigmoid non linearity in the hidden layers
+    #Z is the value at the hidden layer
+    # sigmoid(-(a+b1)) --> sigmoid(-a-b1) hence exp(-X.dot(W1)-b1)
+    Z = 1/(1+np.exp(-X.dot(W1)-b1))
+
+    #Now we can calculate the softmax of the next layer
+    A = Z.dot(W2) +b2
+    #we exponentiate A
+    expA = np.exp(A)
+    #Output Y
+    Y = expA/expA.sum(axis=1,keepdims = True)
+    return Y
+
+#define a function to calculate the classification rate
+#This is gonna take in targets Y and predictions P
+def classification_rate(Y,P):
+    n_correct = 0
+    n_total = 0
+    for i in range(len(Y)):
+        n_total +=1
+        if Y[i]==P[i]:
+            n_correct +=1
+    return float(n_correct)/n_total
+
+'''
+So now we acn actually call the forward function with our randomly initialized weights and calculate the classification rate
+So p_Y_given_X is equal to forward()
+'''
+P_Y_given_X = forward(X,W1,b1,W2,b2)
+# we get the predictions by doing the argmax on P_Y_given_X on axis=1
+Predictions = np.argmax(P_Y_given_X, axis=1)
+
+#now we can assert that we have chosen the right axis to do the argmax
+assert(len(Predictions)==len(Y))
+
+print(f"Classification rate for the randomly chosen weights : {classification_rate(Y,Predictions)}")
